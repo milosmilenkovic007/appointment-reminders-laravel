f6d340ace28fef8d152d3bcd69f3ac4e5d32fe17
Milos <milosmilenkovic007@gmail.com>
2020-04-07 13:53:25 +0200

    css for bulksms fixed

--- resources/views/bulksms.blade.php
+++ resources/views/bulksms.blade.php
@@ -1,6 +1,20 @@
-<html>
-      <body>
-         <form action='' method='post'>
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Send Bulk SMS</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    <form action='' method='post'>
               @csrf
                       @if($errors->any())
               <ul>
@@ -12,14 +26,21 @@
         @if( session( 'success' ) )
              {{ session( 'success' ) }}
         @endif
+        <div class="card">
+        <div class="card-header">Phone numbers (seperate with a comma [,])</div><br>
+        <div class="card-body"> 
+             <input type='text' class="form-control col-xs-12" rows="7" cols="50" name='numbers' /></div>
 
-             <label>Phone numbers (seperate with a comma [,])</label>
-             <input type='text' name='numbers' />
+             <div class="card">
+        <div class="card-header">Message</div><br>
+        <div class="card-body"> 
+            <textarea class="form-control col-xs-12" rows="7" cols="50" name='message'></textarea></div>
 
-            <label>Message</label>
-            <textarea name='message'></textarea>
-
-            <button type='submit'>Send!</button>
+            <button type='submit'>Send Message!</button>
       </form>
-    </body>
-</html>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -8,13 +8,47 @@
     <body>
         <nav class="navbar navbar-default">
             <div class="navbar-header">
-                <a class="navbar-brand" href="#">SMS Reminder System</a>
+            <a class="navbar-brand" href="{{ url('/') }}">
+                    {{ config('app.name', 'SMSReminder') }}
+                </a>
             </div>
             <div class="collapse navbar-collapse">
                 <ul class="nav navbar-nav">
-                    <li><a href="{{ route('appointment.index') }}">Home</a></li>
+                    <li><a href="{{ url('/') }}">Home</a></li>
                     <li><a href="{{ route('appointment.create') }}">New appointment</a></li>
-                </ul>
+                    <li><a href="{{ url('/bulksms') }}">BulkSMS</a></li>
+                
+                <li class="navbar-nav ml-auto">
+                        <!-- Authentication Links -->
+                        @guest
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            </li>
+                            @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                </li>
+                            @endif
+                        @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endguest
+                    </li>
             </div>
         </nav>
         <div class="container">
